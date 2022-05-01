900ea91e44a74415dd3134b0107069bb5a50b675
ouyang888 <757965326@qq.com>
2022-05-01 16:33:37 +0800

    首次提交代码

---
+++ src/router/index.js
@@ -0,0 +1,84 @@
+import Vue from 'vue'
+import store from "../store";
+import VueRouter, {
+    RouteConfig
+} from 'vue-router'
+// import Home from '../views/Home.vue'
+import {
+    getToken
+} from '@/utils/auth';
+import {
+    api
+} from '@/config/index'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: '',
+        redirect: '/center/index'
+    },
+
+
+    {
+        path: '/center',
+        name: 'center',
+        component: () => import('@/views/center/center.vue'),
+        children: [
+            {
+                path: 'index',
+                name: 'index',
+                component: () => import('@/views/center/index/index.vue')
+            },
+            {
+                path: 'domestic',
+                name: 'domestic',
+                component: () => import('@/views/center/domestic/domestic.vue')
+            },
+        ],
+    }
+
+
+]
+const router = new VueRouter({
+    routes
+})
+/**
+ * 路由守卫
+ */
+// router.beforeEach(async (to, from, next) => {
+    // if (to.name !== 'login') {
+    //     const authoritys = await store.dispatch('setUserAuthoritys')
+    //     if (to.fullPath==="/center"&&from.fullPath!=="/center") {
+    //         if (authoritys.includes('admin:quality')) {
+    //             next('/center/qualityList');
+    //             return
+    //         }
+    //     }
+    // if (to.meta.permission) {
+    //     let sign = false
+    //     if (to.meta.permission instanceof Array) {
+    //         to.meta.permission.forEach(item => {
+    //             if (authoritys.indexOf(item) >= 0) {
+    //                 sign = true
+    //             }
+    //         })
+    //     } else if (authoritys.indexOf(to.meta.permission) > -1) sign = true
+
+    //     if (!sign) {
+    //         // if (to.name == 'login') {
+    //         //     console.log("进入coupon权限不足");
+    //         //     // next('/login');
+    //         // } else {
+    //         console.log('权限不足')
+    //         // }
+    //         return;
+    //     }
+    // }
+    // }
+    // next();
+// })
+
+export default router
---
+++ src/views/center/center.vue
@@ -0,0 +1,221 @@
+<template>
+  <div style="background: linear-gradient(180deg, #162770 0%, #1C2B72 41%, #0D182E 100%);background-repeat:no-repeat">
+    <div class="flex-title">
+      <div class="flex-back">
+        <div class="back">
+          <a-icon type="left" style="color: #19ecff" />后退
+        </div>
+        <div class="left-font">当前页面：销向汇总页</div>
+      </div>
+      <div class="flex-right">
+        <div class="right-font">
+          <a-icon
+            type="printer"
+            style="color: #19ecff; margin-right: 4px"
+          />打印
+        </div>
+        <div class="right-font">
+          <a-icon
+            type="upload"
+            style="color: #19ecff; margin-right: 4px"
+          />导出数据
+        </div>
+      </div>
+    </div>
+    <div class="flex-header">
+        
+      <div class="flex-font-left">
+        <div class="head-box"><span>PSI</span></div>
+        <router-link class="head-box active" to="/center/index"><span>S</span></router-link>
+        <div class="head-box"><span>I</span></div>
+        <div class="head-box"><span>P</span></div>
+        <div class="change-flex" style="margin-left: 14px">
+          <div class="change-box"><span>金额版</span></div>
+          <div class="change-box"><span>数量版</span></div>
+        </div>
+      </div>
+      <div class="title-bg">
+        <div class="title">生活电器数智产销中台</div>
+      </div>
+      <div class="flex-font-left">
+        <div class="head-box-right" style="margin-right: 14px">
+          <span
+            >产地<a-icon type="down" style="padding-left: 3px; font-size: 14px"
+          /></span>
+        </div>
+        <div class="change-flex">
+          <div class="head-box-right active"><span>销向</span></div>
+          <div class="head-box-right"><span>产司</span></div>
+        </div>
+        <div class="change-flex">
+          <div class="head-box-right right-width"><span>年度</span></div>
+          <div class="head-box-right right-width"><span>月度</span></div>
+        </div>
+      </div>
+    </div>
+   
+
+   
+  <a-layout-content class="layout-content">
+      <keep-alive include="qualityList,outside,order">
+        <router-view  />  
+      </keep-alive>
+    </a-layout-content>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+    data() {
+      return {
+        searchKeys: [this.$route.path, this.$route.meta.preMenuUrl || ""],
+      }
+    },
+  computed: {
+    titleNav() {
+      return this.$route.meta.title;
+    },
+  },
+  watch: {
+    $route: function (val) {
+      if (val.meta.preMenuUrl || this.$route.path) {
+        this.searchKeys = [this.$route.path, val.meta.preMenuUrl || ""];
+      }
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+.title {
+  font-size: 36px;
+  font-weight: 600;
+  text-align: center;
+  letter-spacing: 8px;
+  color: #fff;
+  width: 100%;
+  position: relative;
+  top: -16px;
+}
+.flex-title {
+  display: flex;
+  justify-content: space-between;
+}
+.left-font {
+  color: #fff;
+  letter-spacing: 2px;
+  font-size: 18px;
+  margin-left: 16px;
+}
+.back {
+  color: #19ecff;
+  font-size: 18px;
+}
+.flex-back {
+  display: flex;
+  margin-left: 16px;
+  white-space: nowrap;
+  margin-top: 10px;
+}
+.flex-right {
+  color: #19ecff;
+  display: flex;
+  font-size: 18px;
+  white-space: nowrap;
+  margin-top: 10px;
+}
+.right-font {
+  margin-right: 16px;
+  cursor: pointer;
+}
+.title-bg {
+  background: url("../../assets/img/navIcon.svg");
+  background-size: 100% 100%;
+  width: 100%;
+  margin: 0 auto;
+}
+.flex-header {
+  display: flex;
+}
+.head-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 68px;
+  height: 44px;
+  transform: skewX(18deg);
+  border: 1px solid #00f9ff;
+  color: #19ecff;
+  background: linear-gradient(0deg, #0e5fff 0%, rgba(28, 0, 255, 0) 100%);
+  font-size: 18px;
+  margin-left: 14px;
+}
+.head-box span {
+  transform: skewX(-18deg);
+}
+.flex-font-left {
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+}
+.head-box {
+  cursor: pointer;
+  opacity: 0.5;
+}
+.head-box:hover {
+  opacity: 1;
+}
+.change-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: skewX(18deg);
+  border: 1px solid #00f9ff;
+  color: #19ecff;
+  background: linear-gradient(0deg, #0e5fff 0%, rgba(28, 0, 255, 0) 100%);
+  font-size: 18px;
+  width: 110px;
+  height: 44px;
+  opacity: 0.5;
+}
+.active {
+  opacity: 1 !important;
+}
+.change-box:hover {
+  opacity: 1;
+}
+.change-box span {
+  transform: skewX(-18deg);
+}
+.change-flex {
+  display: flex;
+  align-items: center;
+  margin-right: 14px;
+}
+.head-box-right {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: skewX(-18deg);
+  border: 1px solid #00f9ff;
+  color: #19ecff;
+  background: linear-gradient(0deg, #0e5fff 0%, rgba(28, 0, 255, 0) 100%);
+  font-size: 18px;
+  width: 110px;
+  height: 44px;
+  cursor: pointer;
+  opacity: 0.5;
+}
+.head-box-right:hover {
+  opacity: 1;
+}
+.head-box-right span {
+  transform: skewX(18deg);
+}
+.right-width {
+  width: 90px !important;
+}
+.change-box {
+  cursor: pointer;
+}
+</style>
